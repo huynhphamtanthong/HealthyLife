f3bdc4559ab0037ba4587568a3de46ef1f3ec25d
Huỳnh Phạm Tân Thông <63225882+huynhphamtanthong@users.noreply.github.com>
2021-09-05 09:21:29 +0700

    Commit lost file

--- HealthyLife/app/src/main/java/com/myapplication/healthylife/model/Dish.java
+++ HealthyLife/app/src/main/java/com/myapplication/healthylife/model/Dish.java
@@ -4,11 +4,25 @@ public class Dish {
     private int ID;
     private String Name;
     private String Type;
+    private String description;
+    private String tutorial;
+    private String note;
+    private String ingredients;
+    private int image;
+    private int video;
 
-    public Dish(int id, String Name, String Type){
+    public Dish(int id, String Name, String Type,
+                String description, String tutorial, String note,
+                String ingredients, int image, int video){
         this.ID=id;
         this.Name= Name;
         this.Type=Type;
+        this.description = description;
+        this.tutorial = tutorial;
+        this.note = note;
+        this.ingredients = ingredients;
+        this.image = image;
+        this.video = video;
     }
 
     public String getType() {
@@ -18,4 +32,28 @@ public class Dish {
     public String getName() {
         return Name;
     }
+    public String getDescription() {
+        return description;
+    }
+
+    public String getTutorial() {
+        return tutorial;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public String getIngredients() {
+        return ingredients;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public int getVideo() {
+        return video;
+    }
+
 }
