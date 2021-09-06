7cbeed5b40b781e91199a2ba0bbb8e21386b2557
vt132 <54886268+vt132@users.noreply.github.com>
2021-09-06 14:41:01 +0700

    update data

--- HealthyLife/app/src/main/java/com/myapplication/healthylife/fragments/firstusefragment/FirstUseFragment.java
+++ HealthyLife/app/src/main/java/com/myapplication/healthylife/fragments/firstusefragment/FirstUseFragment.java
@@ -209,32 +209,32 @@ public class FirstUseFragment extends Fragment {
         dishes.add(new Dish(-1, "Bacon and egg"," No Description",
                 "Fried Bacon and egg in medium heat in 3 mins, until it is done",
                 "Eat with green veggies for fiber + vitamins","2-3 eggs + the amount of bacon you want",
-                1,1,true, false, false, true,false, false));
+                R.drawable.eggnbacon,1,true, false, false, true,false, false));
         dishes.add(new Dish(-1, "Avocado salad"," No Description",
                 "Prepare and wash lettuce, avocado, bell chillies, cucumber. Mix them all together with  Vinaigrette",
                 "Non-vegan can top up with eggs  for diversity","Lettuce,cucumber, avocados, bell chillies, Vinaigrette  ",
-                2,2,false, false, true, false,false, true));
+                R.drawable.avocadosalad,2,false, false, true, false,false, true));
         dishes.add(new Dish(-1, "Nuts milk with chia seed"," No Description",
                 "Use any types of nuts milk such as almond milk, soy milk, walnuts milk... and drop 1 tablespoon of chia seed in",
                 "Eat with fruits(apples, strawberries) if you like","Chia Seed, nuts milk"  ,
-                3,3,true, false, true, false,true, true));
+                R.drawable.milknchia,3,true, false, true, false,true, true));
         dishes.add(new Dish(-1, "Green pea with tofu/beef  ","No Description ",
                 "Wash green pea, season tofu/beef with garlics, soy bean. After stir frying tofu/beef for 2-3 min with medium/high flame, add the green peas, continue stirring for 5 min and turr off the heat.",
                 "Non-vegan can cook and eat with beef, Vegan can do the same with tofu, people with no restriction in carb can eat with rice",
                 "Green peas, tofu/beef, soy sauce, garlic, ",
-                4,4,true, false, true, false,false,true));
+                R.drawable.peasntofu,4,true, false, true, false,false,true));
         dishes.add(new Dish(-1, "Omelet with veggies","No Description ",
                 "Break eggs, fry with olive oil for few mins. Meanwhile prepare boiled veggies (carrot,raddish, brocoli) ",
                 "No note","Eggs, vegetables ( carrot/brocoli/etc...) ",
-                5,5,true, false, false, true,false, false));
+                R.drawable.eggnveg,5,true, false, false, true,false, false));
         dishes.add(new Dish(-1, "(Nuts) Yogurt with strawberries and nuts","No Description ",
                 "Just mix the yogurt with berries and nuts (any type you want)",
                 "Non-vegan can have cow milk yogurt, Vegan can alternate with nuts yogurt","Yogurt(nuts or normal), strawberries/blueberries, nuts(almond, walnuts...) ",
-                6,6,true, false,true, true,true, false));
+                R.drawable.yogurtstrawberries,6,true, false,true, true,true, false));
         dishes.add(new Dish(-1, "Pork chop with veggies  ","No Description ",
                 "wash pork and boil it with salt (1 coffee spoon) until bubbles come up, after that countinue doing so for 15 min(or until the red part inside is gone).Meanwhile wash any kind of green veggies (cabbage, amaranth,brocoli...) and boil it after finishing the meat.",
                 "No note","Pork, favorite veggies",
-                7,7,true, false, false, false,true, true));
+                R.drawable.porknveg,7,true, false, false, false,true, true));
     }
     private void saveListofDietForNewUser(ArrayList<Diet> diets, double bmi){
         boolean startRecommended = false;
