e3388367f92cbd8928edde94f1f00b92e147450c
Phát Hữu Châu <chauhuuphat21062001@gmail.com>
2021-09-06 12:27:41 +0700

    Drink Water Noti + Update About BMI Noti + Update Layout Stan-up Noti

--- HealthyLife/app/src/main/java/com/myapplication/healthylife/DrinkWaterNoti.java
+++ HealthyLife/app/src/main/java/com/myapplication/healthylife/DrinkWaterNoti.java
@@ -1,4 +1,4 @@
-package com.myapplication.healthylife.fragments.mainfragments;
+package com.myapplication.healthylife;
 
 import android.app.AlarmManager;
 import android.app.PendingIntent;
@@ -16,31 +16,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.myapplication.healthylife.AlarmReceiver;
-import com.myapplication.healthylife.R;
-
 import java.util.Calendar;
 
-public class StandUpNoti extends Fragment {
+public class DrinkWaterNoti extends Fragment {
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View v = inflater.inflate(R.layout.fragment_stand_up_noti, container, false);
-        return v;
+            // Inflate the layout for this fragment
+            View v = inflater.inflate(R.layout.fragment_drink_water_noti, container, false);
+            return v;
     }
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         AlarmManager alarmManager = (AlarmManager) getActivity(). getSystemService(Context.ALARM_SERVICE);
-        Intent notificationIntent = new Intent(getActivity(), AlarmReceiver.class);
+        Intent notificationIntent = new Intent(getActivity(), AlarmReceiver3.class);
         PendingIntent broadcast = PendingIntent.getBroadcast(getActivity(), 100, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
         Calendar cal = Calendar.getInstance();
 
         Switch simpleSwitch = (Switch) view.findViewById(R.id.switch1);
         SharedPreferences sharedPrefs = getActivity().getPreferences(Context.MODE_PRIVATE);
-        simpleSwitch.setChecked(sharedPrefs.getBoolean("Standup", false));
+        simpleSwitch.setChecked(sharedPrefs.getBoolean("Water", false));
         simpleSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -55,17 +53,16 @@ public class StandUpNoti extends Fragment {
         simpleSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(simpleSwitch.isChecked())  {
+                if (simpleSwitch.isChecked()) {
                     SharedPreferences.Editor editor = sharedPrefs.edit();
-                    editor.putBoolean("Standup", true);
+                    editor.putBoolean("Water", true);
+                    editor.commit();
+                } else {
+                    SharedPreferences.Editor editor = sharedPrefs.edit();
+                    editor.putBoolean("Water", false);
                     editor.commit();
                 }
-                else{
-                    SharedPreferences.Editor editor = sharedPrefs.edit();
-                    editor.putBoolean("Standup", false);
-                    editor.commit();                }
             }
         });
-
     }
 }
